f8ee5062937f011a6884c8048df40d459d58da3d
Luke Chen <luke.chen.2010@gmail.com>
2020-07-20 02:41:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+document.addEventListener( "DOMContentLoaded", function () 
+{
+  {
+    document.getElementById( "text" ).innerHTML = "This is really cool!";
+  }
+} );
+
+
+function updateDOM() 
+{
+  
+}
